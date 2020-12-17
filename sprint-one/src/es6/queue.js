245d30c0e16d448024ec08810c684a4eace7e30a
Aschale <aschaled@gmail.com>
2020-12-17 12:29:18 -0500

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,31 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  enqueue(val) {
+    this.storage[this.counter] = val;
+    this.counter++;
+  }
+
+  dequeue() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    var temp = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[this.counter];
+    return temp;
+  }
+
+  size() {
+    return this.counter;
+  }
 }
+
+var queue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  push(val) {
+    this.storage[this.counter] = val;
+    this.counter++;
+  }
+
+  pop() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    return this.storage[this.counter];
+  }
+
+  size() {
+    return this.counter;
+  }
 }
+
+var stack = new Stack();
