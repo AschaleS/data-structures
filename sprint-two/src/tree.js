3fdbeff31a75c743f0f218b450bff9a9eeb3e19b
Aschale <aschaled@gmail.com>
2021-01-05 23:36:03 -0500

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +12,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, node) {
+  var currentNode = node || this;
+  if (currentNode.value === target) {
+    return true;
+  }
+  for (var i = 0; i < currentNode.children.length; i++) {
+    if (this.contains(target, currentNode.children[i])) {
+      return true;
+    }
+  }
+  return false;
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ The time complexity for the addChild method is O(1) (constant), because there is no need to iterate over the array.
+
+ The time complexity for contains is O(n) (linear), because as the number of nodes to check increases, so does the time spent checking each node individually.
  */
