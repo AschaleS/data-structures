4662fc50e215b1fb7519425ddf6834a490c50cd9
Aschale <aschaled@gmail.com>
2021-01-07 21:14:17 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ Both the add and remove methods are O(1) (constant) because they do not need to iterate over the set of values
+
+ The contains method is O(n) because it has to iterate over each item in the object to determine if the item exists in the specific object.
  */
